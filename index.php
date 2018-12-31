37d609dbd4822ec5a058f00237d415ff1d0c6a07
msmsupto <46277383+msmsupto@users.noreply.github.com>
2018-12-31 15:09:19 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+ header('Content-Type: text/html; charset=utf-8');
+$array = json_decode(file_get_contents("http://free.currencyconverterapi.com/api/v5/convert?q=$_GET[from]_$_GET[to]&compact=y"),TRUE);
+$value = $array["$_GET[from]_$_GET[to]"][val] * $_GET[amount];
+$rep  = array(
+		'messages' => array(
+				0 => array(
+						'text' => "$_GET[amount] $_GET[from] = $value $_GET[to]"
+				)
+		)
+);
+echo json_encode($rep);
+array(
+'Helo' => 123,
+'Abcjskjkajska' => 83483);
+?>
